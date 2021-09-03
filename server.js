4cba95ba009182a5df4fab4b2484b38407095fb4
Victor Teixeira <victor.teixeira@kumulus.com.br>
2021-09-02 23:38:41 -0300

    Dockerfile .env14

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
-require('dotenv').config();
+
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -16,7 +16,7 @@ var Message = mongoose.model('Message',{
 })
 
 
-var dbUrl = process.env.MONGO_CONNECTION
+var dbUrl = "mongodb+srv://victor:mZQCxSRX1cUqTTAt@cluster0.afhhe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 app.get('/messages', (req, res) => {
   Message.find({},(err, messages)=> {
