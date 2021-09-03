79ee584c68d2da0c039db0b4aff528f155c62b68
Victor Teixeira <victor.teixeira@kumulus.com.br>
2021-09-02 23:15:30 -0300

    Dockerfile .env11

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
-require('custom-env').env("dev");
+require('dotenv').config();
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
